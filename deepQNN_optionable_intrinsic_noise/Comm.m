4c9128c7ba952e4aaf6fc8255ae4cf21930c3964
QIG ITP Hannover <56720711+qigitphannover@users.noreply.github.com>
2022-01-27 13:02:16 +0100

    Delete deepQNN_optionable_intrinsic_noise directory

--- deepQNN_optionable_intrinsic_noise/Comm.m
+++
@@ -1,6 +0,0 @@
-%Calculates Commutator of matrix A and B
-function C = Comm(A,B)
-
-C = A*B -B*A;
-
-end
--- deepQNN_optionable_intrinsic_noise/SpectralRange.m
+++
@@ -1,4 +0,0 @@
-function range = SpectralRange(A)
-%Outputs a spectral gap of a matrix A.
-
-    range = norm(norm(A)*eye(size(A,1)) - A);
